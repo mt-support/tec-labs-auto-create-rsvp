15ceeb1ccb1735972d00e45d88ca415522b9776e
Matthew Batchelder <borkweb@gmail.com>
2023-06-24 10:00:02 -0400

    Update to use the new Service_Provider contract in common.

--- src/Tec/PUE.php
+++ src/Tec/PUE.php
@@ -9,6 +9,7 @@
 
 namespace Tribe\Extensions\Autocreate_RSVP;
 
+use TEC\Common\Contracts\Service_Provider;
 use Tribe__PUE__Checker;
 
 /**
@@ -18,7 +19,7 @@ use Tribe__PUE__Checker;
  *
  * @package Tribe\Extensions\Autocreate_RSVP;
  */
-class PUE extends \tad_DI52_ServiceProvider {
+class PUE extends Service_Provider {
 
 	/**
 	 * The slug used for PUE.
